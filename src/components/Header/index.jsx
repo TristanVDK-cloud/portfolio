a9d6e79c9874e2a2c16107b2524e9b50fb838215
TristanVDK <calgara@me.com>
2026-08-24 13:19:26 +0200

    add Header component with styles

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,23 @@
+import './Header.scss'
+
 function Header() {
     return (
-        <div>Header en cours...</div>
+        <header className='header'>
+            <div className='header__container'>
+                <a href="#home" className='header__logo'>
+                    T<span>C</span>
+                </a>
+                <nav className='header__nav'>
+                    <ul>
+                        <li><a href="#home">Accueil</a></li>
+                        <li><a href="#about">À propos</a></li>
+                        <li><a href="#projects">Projets</a></li>
+                        <li><a href="#experience">Parcours</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
     );
 }
 
