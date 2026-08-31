ca3e156ab735b6e541e550b89d3312f02d79e18a
TristanVDK <calgara@me.com>
2026-08-31 09:34:03 +0200

    add Projects component, his style and animations, including Modal component for project details

---
+++ src/components/Projects/index.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+import projectsData from '../../data/projects.json';
+import './Projects.scss';
+
+function Projects() {
+    const [activeFilter, setActiveFilter] = useState('Tous');
+    const categories = ['Tous', 'HTML', 'CSS', 'React', 'Sass', 'JavaScript'];
+    const [selectedProject, setSelectedProject] = useState(null);
+    const filteredProjects = activeFilter === 'Tous'
+        ? projectsData
+        : projectsData.filter(project => project.stack.includes(activeFilter));
+
+    return (
+        <section className='projects' id="projects">
+            <div className='projects__container'>
+                <h2 className='projects__title'>Mes Projets</h2>
+                <div className='projects__filters'>
+                    {categories.map(cat => (
+                        <button
+                            key={cat}
+                            className={`filter-btn ${activeFilter === cat ? 'active' : ''}`}
+                            onClick={() => setActiveFilter(cat)}
+                        >
+                            {cat}
+                        </button>
+                    ))}
+                </div>
+                <div className='projects__grid'>
+                    {filteredProjects.map(project => (
+                        <div
+                            key={project.id}
+                            className='project-card'>
+                            <div className='project-card__inner' onClick={() => setSelectedProject(project)}>
+                                <div className='project-card__image-wrapper'>
+                                    <img src={project.cover} alt={project.title} />
+                                    <div className='project-card__overlay'>
+                                        <span>Voir en détail</span>
+                                    </div>
+                                </div>
+                                <div className='project-card__info'>
+                                    <h3>{project.title}</h3>
+                                    <p>{project.description}</p>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                {/* <Modal
+                    project={selectedProject}
+                    onClose={() => setSelectedProject(null)}
+                /> */}
+            </div>
+        </section>
+    );
+}
+
+export default Projects;
